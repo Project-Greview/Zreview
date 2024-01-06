ceea4899403227e6d9a2834ee75e794bdfee3357
kkt9102 <kkt9102@gmail.com>
2024-01-07 01:43:08 +0900

    feat : login end

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,34 +1,49 @@
 // MODULE
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 // IMAGE
 import { ReactComponent as Logo } from "../../assets/image/Logo.svg";
 // PROPS TYPE
 type LoginProps = {};
 
 const Login: React.FC<LoginProps> = () => {
+  const navigate = useNavigate();
   const [isReady, setIsReady] = useState(false);
 
+  // ENABLE LOGIN BOX
   const handleOnLoginBox = () => {
     setIsReady(true);
   };
+
+  // LOGIN
+  const handleLogin = () => {};
   return (
     <div
       className="con flex flex_dir_c flex_jc_c flex_ai_c mar_top_50"
-      style={{ marginTop: "15% " }}
+      style={{ marginTop: "10% " }}
     >
       <Logo />
       <div className={`login_box ${!isReady ? "" : "active"}`}>
         <div className="input_box">
-          <input type="text" />
+          <input
+            type="text"
+            className="input_default width_100p"
+            placeholder="아이디"
+          />
           <label htmlFor=""></label>
         </div>
         <div className="input_box">
-          <input type="text" />
+          <input
+            type="text"
+            className="input_default width_100p"
+            placeholder="비밀번호"
+          />
           <label htmlFor=""></label>
         </div>
       </div>
-      <div>
+      <div className="btn_box flex">
         <div
+          className="buttons flex flex_jc_c flex_ai_c width_100p cursor_p"
           onClick={
             !isReady ? () => handleOnLoginBox() : () => console.log("로그인")
           }
@@ -36,9 +51,14 @@ const Login: React.FC<LoginProps> = () => {
           {!isReady ? "시작하기" : "로그인"}
         </div>
       </div>
-      <div className="flex">
-        <div>처음이신가요?</div>
-        <div>회원가입</div>
+      <div className="registraion_info flex">
+        <div>처음이신가요?&ensp;</div>
+        <div
+          className="buttons cursor_p"
+          onClick={() => navigate("registration")}
+        >
+          회원가입
+        </div>
       </div>
     </div>
   );
