65694ddb0842cb5f23d3428a1451533ea20f5f2b
kkt9102 <kkt9102@gmail.com>
2024-01-09 07:13:09 +0900

    feat :navigate icon add

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,9 +1,21 @@
 // MODULE
-
+import { useNavigate, useLocation } from "react-router-dom";
+// SVG
+import { ReactComponent as HomeIcon } from "../../assets/image/icon/Map-icon.svg";
 // RPORS TYPE
 type NavigationProps = {};
 const Navigation: React.FC<NavigationProps> = () => {
-  return <></>;
+  const navigate = useNavigate();
+  const location = useLocation();
+  return (
+    <div className="navigate flex flex_jc_sb flex_ai_c">
+      <ul className="flex flex_jc_sb flex_ai_c">
+        <li>
+          <HomeIcon />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navigation;
