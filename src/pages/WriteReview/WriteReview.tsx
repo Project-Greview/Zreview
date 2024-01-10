fa68c026cb1d6174b6cb44c209a1e3724b76b295
ki tae kim <kitaekim@kiui-MacBookPro.local>
2024-01-10 16:53:54 +0900

    feat : style edit

--- src/pages/WriteReview/WriteReview.tsx
+++ src/pages/WriteReview/WriteReview.tsx
@@ -9,11 +9,15 @@ const WriteReview: React.FC<WriteReviewProps> = () => {
       <div className="write_form">
         <div className="location_select">
           <div className="section_title">장소명 선택</div>
-          <div className="radio_box">
-            <input type="radio" id="location_search" name="location_type" />
-            <label htmlFor="location_search">장소검색</label>
-            <input type="radio" id="write_location" name="location_type" />
-            <label htmlFor="write_location">직접입력</label>
+          <div className="radio_box flex">
+            <div>
+              <input type="radio" id="location_search" name="location_type" />
+              <label htmlFor="location_search">장소검색</label>
+            </div>
+            <div>
+              <input type="radio" id="write_location" name="location_type" />
+              <label htmlFor="write_location">직접입력</label>
+            </div>
           </div>
         </div>
       </div>
