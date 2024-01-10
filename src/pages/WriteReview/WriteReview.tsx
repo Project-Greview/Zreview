15f2519370585a5c783959da094d93d5326e3d65
kkt9102 <kkt9102@gmail.com>
2024-01-10 21:24:15 +0900

    feat : style edit

--- src/pages/WriteReview/WriteReview.tsx
+++ src/pages/WriteReview/WriteReview.tsx
@@ -1,5 +1,7 @@
 // MODULE
-
+import { useEffect, useState } from "react";
+// SVG
+import { ReactComponent as SearcIcon } from "../../assets/image/icon/keyword_search.svg";
 // PROPS TYPE
 type WriteReviewProps = {};
 
@@ -10,15 +12,30 @@ const WriteReview: React.FC<WriteReviewProps> = () => {
         <div className="location_select">
           <div className="section_title">장소명 선택</div>
           <div className="radio_box flex">
-            <div>
+            <div className="mar_rh_5">
               <input type="radio" id="location_search" name="location_type" />
-              <label htmlFor="location_search">장소검색</label>
+              <label htmlFor="location_search" className="flex">
+                장소 검색
+              </label>
             </div>
             <div>
               <input type="radio" id="write_location" name="location_type" />
-              <label htmlFor="write_location">직접입력</label>
+              <label htmlFor="write_location" className="flex">
+                직접 입력
+              </label>
             </div>
           </div>
+        </div>
+        <div className="search_section relative flex flex_jc_sb flex_ai_c">
+          <input
+            type="text"
+            className="keyword_input "
+            placeholder="장소명을 입력해주세요"
+          />
+          <label htmlFor="" className="absolute">
+            <SearcIcon />
+          </label>
+          <div className="btn flex flex_jc_c flex_ai_c">확인</div>
         </div>
       </div>
     </div>
