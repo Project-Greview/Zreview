45b92680d72438bb9fda680188e1c898888075a8
kkt9102 <kkt9102@gmail.com>
2024-01-23 07:09:35 +0900

    feat : leftMenu

--- src/components/LeftMenu/LeftMenu.tsx
+++ src/components/LeftMenu/LeftMenu.tsx
@@ -1,5 +1,6 @@
 // MODULE
 import { useRecoilState } from "recoil";
+import { useNavigate } from "react-router-dom";
 // RECOIL STATE
 import { leftMenuState } from "state/userState";
 // SVG
@@ -13,21 +14,37 @@ import { ReactComponent as ArrowIcon } from "../../assets/image/icon/arrow_right
 import ProfileImage from "components/ProfileImage";
 
 const MiddleMenu: React.FC = () => {
+  const navigate = useNavigate();
+  // STATE
+  const [leftMenu, setLeftMenu] = useRecoilState(leftMenuState);
+  const handleMovePage = (url: string) => {
+    navigate(url);
+    setLeftMenu(false);
+  };
   return (
     <ul>
-      <li className="flex flex_jc_sb flex_ai_c">
+      <li
+        className="flex flex_jc_sb flex_ai_c"
+        onClick={() => handleMovePage("/mylocation")}
+      >
         <MyLocationIcon color={"#959292"} />
-        <div>내 동네 탐색</div>
+        <div className="txt flex_flex_jc_s">내 동네 탐색</div>
         <ArrowIcon />
       </li>
-      <li className="flex flex_jc_sb flex_ai_c">
+      <li
+        className="flex flex_jc_sb flex_ai_c"
+        onClick={() => handleMovePage("/write")}
+      >
         <ReviewIcon color={"#959292"} />
-        <div>리뷰 쓰기</div>
+        <div className="txt flex_flex_jc_s">리뷰 쓰기</div>
         <ArrowIcon />
       </li>
-      <li className="flex flex_jc_sb flex_ai_c">
+      <li
+        className="flex flex_jc_sb flex_ai_c"
+        onClick={() => handleMovePage("/bookmark")}
+      >
         <BookMarkIcon color={"#959292"} />
-        <div>저장 목록</div>
+        <div className="txt flex_flex_jc_s">저장 목록</div>
         <ArrowIcon />
       </li>
     </ul>
@@ -58,6 +75,18 @@ const LeftMenu: React.FC<LeftMenuProps> = () => {
           <ProfileImage src={""} alt={"프로필"} size={50} />
           <div className="my_nickname">여기에는 내 닉네임이</div>
           <div className="my_location">여기에는 내 설정지역이</div>
+        </div>
+        <div className="semi_count">
+          <ul className="flex flex_jc_sb flex_ai_c">
+            <li>
+              <div>리뷰</div>
+              <div>00</div>
+            </li>
+            <li>
+              <div>댓글</div>
+              <div>00</div>
+            </li>
+          </ul>
         </div>
       </div>
       <div className="middle_section">
