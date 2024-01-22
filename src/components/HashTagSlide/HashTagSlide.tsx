b5b13a743be70c5ae842ef3cea368ff274af854b
kkt9102 <kkt9102@gmail.com>
2024-01-23 06:33:41 +0900

    feat : slide tag click active add

--- src/components/HashTagSlide/HashTagSlide.tsx
+++ src/components/HashTagSlide/HashTagSlide.tsx
@@ -1,4 +1,5 @@
 // MODULE
+import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Swiper as SwiperCore } from "swiper";
 import "swiper/css";
@@ -8,6 +9,10 @@ import { ReactComponent as MarkerIcon } from "../../assets/image/icon/marker_c.s
 type HashTagSlideProps = {};
 
 const HashTagSlide: React.FC<HashTagSlideProps> = () => {
+  const [activeTag, setActiveTag] = useState<Number>(-1);
+  const handleClickHashtag = (index: Number) => {
+    setActiveTag(index === activeTag ? -1 : index);
+  };
   return (
     <div className="main_tag_slide absolute">
       <Swiper
@@ -16,23 +21,38 @@ const HashTagSlide: React.FC<HashTagSlideProps> = () => {
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper: SwiperCore) => console.log(swiper)}
       >
-        <SwiperSlide>
+        <SwiperSlide
+          onClick={() => handleClickHashtag(1)}
+          className={`${1 === activeTag ? "active_tag" : ""}`}
+        >
           <MarkerIcon />
           <div className="txt">텍스트</div>
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide
+          onClick={() => handleClickHashtag(2)}
+          className={`${2 === activeTag ? "active_tag" : ""}`}
+        >
           <MarkerIcon />
           <div className="txt">텍스트</div>
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide
+          onClick={() => handleClickHashtag(3)}
+          className={`${3 === activeTag ? "active_tag" : ""}`}
+        >
           <MarkerIcon />
           <div className="txt">텍스트</div>
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide
+          onClick={() => handleClickHashtag(4)}
+          className={`${4 === activeTag ? "active_tag" : ""}`}
+        >
           <MarkerIcon />
           <div className="txt">텍스트</div>
         </SwiperSlide>
-        <SwiperSlide>
+        <SwiperSlide
+          onClick={() => handleClickHashtag(5)}
+          className={`${5 === activeTag ? "active_tag" : ""}`}
+        >
           <MarkerIcon />
           <div className="txt">텍스트</div>
         </SwiperSlide>
