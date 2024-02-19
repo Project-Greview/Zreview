34a6e06da1a8ad58e84132185c1cfb0680c19f1b
kkt9102 <kkt9102@gmail.com>
2024-02-19 13:42:39 +0900

    feat : dummy data modal css.module 적용

--- src/components/DummyModal/DummyModal.tsx
+++ src/components/DummyModal/DummyModal.tsx
@@ -8,7 +8,7 @@ import { generateRandomData } from "utils/dummyReview";
 // SVG
 import { ReactComponent as CloseIcon } from "../../assets/image/icon/close_btn.svg";
 // CUSTOM CSS
-import "../../assets/styles/custommodal.css";
+import dummy from "../../assets/styles/custommodal.module.css";
 import Button from "components/Common/Button";
 import Input from "components/Common/Input";
 // PROPS TYPE
@@ -46,21 +46,25 @@ const DummyModal: React.FC<DummyModalType> = ({ close }) => {
   return (
     <>
       <div className="modal_bg fixed"></div>
-      <div className="modal_frame fixed" style={{ minHeight: "45vh" }}>
-        <div className="modal_header flex flex_jc_sb flex_ai_c">
-          <div className="title flex flex_jc_c">더미데이터 만들기</div>
+      <div className={`${dummy.modal_frame} modal_frame fixed`}>
+        <div
+          className={`${dummy.modal_header} modal_header flex flex_jc_sb flex_ai_c`}
+        >
+          <div className={`${dummy.title} title flex flex_jc_c`}>
+            더미데이터 만들기
+          </div>
           <div className="close_btn" onClick={close}>
             <CloseIcon width={20} height={20} />
           </div>
         </div>
-        <div className="modal_body">
-          <ul className="explanation_list">
+        <div className={`${dummy.modal_body} modal_body`}>
+          <ul className={dummy.explanation_list}>
             <li>임시로 DummyData를 생성하기위한 Modal 입니다.</li>
             <li>원하시는 Data 갯수를 입력 후 생성하기 버튼을 눌러주세요!</li>
             <li>1 ~ 200 사이의 숫자를 입력해주세요.</li>
             <li>DummyData는 생생 시 마다 초기화됩니다.</li>
           </ul>
-          <div className="count_box">
+          <div className={`${dummy.count_box} count_box`}>
             <Input
               id={"data_count"}
               name={""}
@@ -74,7 +78,7 @@ const DummyModal: React.FC<DummyModalType> = ({ close }) => {
               styles={""}
             />
           </div>
-          <div className="btn_box relative flex flex_jc_sb">
+          <div className={`${dummy.btn_box} btn_box relative flex flex_jc_sb`}>
             <Button
               title={"생성하기"}
               width={"45%"}
