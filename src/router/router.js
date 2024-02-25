9923767e90e101ccd6279320debbb64e39c266d6
kkt9102 <kkt9102@gmail.com>
2024-02-25 14:42:01 +0900

    feat : router edit

--- src/router/router.js
+++ src/router/router.js
@@ -132,7 +132,7 @@ const Routers = createBrowserRouter(routes, {
   // basename: window.location.hostname === "localhost" ? "/" : process.env.PUBLIC_URL,
   basename:
     // process.env.NODE_ENV === "production" ? process.env.PUBLIC_URL : "/",
-    process.env.NODE_ENV === "production" ? "/" : "/",
+    "/",
 });
 
 export default Routers;
