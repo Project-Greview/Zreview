3037dd100cc07261f28883ac170dd093c9581794
kkt9102 <kkt9102@gmail.com>
2024-02-26 15:40:51 +0900

    feat : PC View 소개페이지 section 6 시작 전 커밋

--- src/components/PCView/Section2/Section2.tsx
+++ src/components/PCView/Section2/Section2.tsx
@@ -1,11 +1,49 @@
 const Section2: React.FC = () => {
   return (
-    <div className="pc_section section_1">
-      <div className="img_box">
-        <img
-          src={`${process.env.PUBLIC_URL}/assets/image/bg_logo.png`}
-          alt={`지리뷰(Zreview) 로고`}
-        />
+    <div className="pc_section section_2">
+      <div className="pc_con flex flex_dir_c flex_jc_c flex_ai_c">
+        <h6 className="point_txt">Overview</h6>
+        <h2>
+          <span>‘집 근처’의 가치</span>가 주목받는 시대
+        </h2>
+        <p>
+          핫플 말고 살기 좋은 곳. 그렇다고 해서 맛집이나 인스타용 카페 같은
+          핫플만을 공략한 건 아닌 머물고 싶은 우리동네가 뜬다
+          <br />
+          가게를 운영하는 사람들도 결국 주민이다. 일하는 환경이 좋아야 하지만 그
+          외 시간에도 살기 괜찮은 곳이어야 한다.
+          <br />
+          따듯한 공동체도 형성돼 있어야 떠나지 않는다.
+        </p>
+        <ul className="circle_chart flex flex_jc_c flex_ai_c">
+          <li className="circle_box flex flex_dir_c flex_jc_c flex_ai_c">
+            <div className="flex flex_dir_c flex_jc_c flex_ai_c">
+              <strong className="point_txt">Hyperlocal</strong>
+              <div className="point_txt">(하이퍼로컬)</div>
+            </div>
+            <p>
+              극단을 뜻하는 하이퍼(Hyper)와
+              <br />
+              지역을 의미하는 (Local)을 더한 합성어로
+              <br />
+              동네를 중심으로 이뤄지는 네트워킹
+            </p>
+          </li>
+          <li className="plus_txt point_txt">+</li>
+          <li className="circle_box flex flex_dir_c flex_jc_c flex_ai_c">
+            <div className="flex flex_dir_c flex_jc_c flex_ai_c">
+              <strong className="point_txt">Vertical</strong>
+              <div className="point_txt">(버티컬서비스)</div>
+              <p>
+                이용자들의 수요와 취향이 더욱
+                <br />
+                다양해지고 구체화되면서,
+                <br />
+                수요맞춤형 서비스의 중요성 증가
+              </p>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
